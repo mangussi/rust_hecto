5fd6f1be5fe38db375b0f9d6e32c75786474a3b6
Fernando Mangussi <fernando.mangussi@teck.com>
2024-03-30 08:07:33 -0600

    hecto

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+mod document;
+mod editor;
+mod row;
+mod terminal;
+pub use document::Document;
+pub use editor::Position;
+pub use row::Row;
+pub use terminal::Terminal;
+
+fn main() {
+    editor::Editor::default().run();
+}
